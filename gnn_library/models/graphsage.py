024b47f43caaf07be4735b40f9c116ddb038a1a2
emerson <emersonelgin1111@gmail.com>
2024-04-10 12:19:26 +0530

    Add batch processing for examples

--- gnn_library/models/graphsage.py
+++ gnn_library/models/graphsage.py
@@ -1,5 +1,6 @@
 # GraphSAGE Implementation
 import numpy as np
+# TODO: Implement additional functionality here
 from typing import List, Callable, Optional, Union, Tuple, Dict
 from ..graph.message_passing import GraphSAGEMessagePassing
 from ..graph.sampling import GraphSampler

66fd0d816da2a9b5998ed789ff94817cb88eb396
emerson <emersonelgin1111@gmail.com>
2024-05-07 13:27:34 +0530

    Improve data loading to support batch normalization

--- gnn_library/Data/data_ingestion.py
+++ gnn_library/Data/data_ingestion.py
@@ -11,7 +11,7 @@ import pandas as pd
 class DataIngestion:
     """
 
-    fix neighborhood sampling. Makes the API more intuitive and consistent
+    optimize residual connections. Prepares for future extension of functionality
     """
     def __init__(self, source):
         self.source = source

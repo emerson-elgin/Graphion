5493b350dee2f0d266d008a3352ac3def5ac40a4
emerson <emersonelgin1111@gmail.com>
2024-05-17 11:22:46 +0530

    Extend documentation: edge cases and edge cases

--- gnn_library/graph/dynamic.py
+++ gnn_library/graph/dynamic.py
@@ -1,6 +1,7 @@
 # Updated: optimize performance issues. Reduces memory footprint for large graphs
 # debug large graph support issues
 class DynamicGraph:
+# TODO: Implement additional functionality here
     """
 
     improve multi-head attention mechanism

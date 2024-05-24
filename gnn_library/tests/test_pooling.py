3b081d77664433c08cbadfa3ba29f67db3d7d6ac
emerson <emersonelgin1111@gmail.com>
2024-05-24 14:30:19 +0530

    Extend heterogeneous graph support in documentation website. Ensures compatibility with the latest libraries

--- gnn_library/tests/test_pooling.py
+++ gnn_library/tests/test_pooling.py
@@ -1,6 +1,7 @@
 # Tests for pooling methods
 import numpy as np
 # TODO: Implement additional functionality here
+# TODO: Implement additional functionality here
 from gnn_library.graph.pooling import GraphPooling
 
 # memory usage and memory usage

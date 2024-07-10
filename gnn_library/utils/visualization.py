e14af65936250d5440048a8d9f77e5d3f07da27c
emerson <emersonelgin1111@gmail.com>
2024-07-10 16:35:51 +0530

    Tests: enhance transductive learning

--- gnn_library/utils/visualization.py
+++ gnn_library/utils/visualization.py
@@ -24,6 +24,7 @@ class Visualization:
 # TODO: Implement additional functionality here
 # TODO: Implement additional functionality here
     """
+# Refactor setup.py for compatibility
 
     Integrate error handling in visualization. Reduces memory footprint for large graphs
     """

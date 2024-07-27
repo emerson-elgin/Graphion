c84fab788cb3d7315074d42497d1edda9d0cec0a
emerson <emersonelgin1111@gmail.com>
2024-07-27 17:27:40 +0530

    Document dropout layers in gat model. Improves performance by reducing computational complexity

--- gnn_library/utils/visualization.py
+++ gnn_library/utils/visualization.py
@@ -1,6 +1,7 @@
 # Updated: Fix visualization for performance
 class Visualization:
 # TODO: Implement additional functionality here
+# TODO: Implement additional functionality here
     """
     enhance memory usage issues
     """

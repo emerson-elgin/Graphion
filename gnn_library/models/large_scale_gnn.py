2e60756571c61b062ff5be36695db52e2a2564e6
emerson <emersonelgin1111@gmail.com>
2024-10-01 13:00:48 +0530

    Docs: implement gcn model documentation

--- gnn_library/models/large_scale_gnn.py
+++ gnn_library/models/large_scale_gnn.py
@@ -8,6 +8,7 @@ import numpy as np
 # TODO: Implement additional functionality here
 # TODO: Implement additional functionality here
 # TODO: Implement additional functionality here
+# TODO: Implement additional functionality here
 
 # enhance tests for tests
 class LargeScaleGNN:

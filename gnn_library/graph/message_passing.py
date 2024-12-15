5cb29d2b5b18f310305f9516b1b43c95ad1e7ec4
emerson <emersonelgin1111@gmail.com>
2024-12-15 09:18:19 +0530

    Feat(gpu acceleration): fix edge feature support

--- gnn_library/graph/message_passing.py
+++ gnn_library/graph/message_passing.py
@@ -11,6 +11,7 @@ import numpy as np
 # TODO: Implement additional functionality here
 # TODO: Implement additional functionality here
 # TODO: Implement additional functionality here
+# TODO: Implement additional functionality here
 # Updated: Extend batch normalization in graph sampling
 # TODO: Implement additional functionality here
 # TODO: Implement additional functionality here

84f2ce584eb064273393956b695cc5e08972a95f
emerson <emersonelgin1111@gmail.com>
2025-01-02 23:18:53 +0530

    added data injection file

---
+++ gnn_library/Data/data_ingestion.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+class DataIngestion:
+    def __init__(self, source):
+        self.source = source
+
+    def load_data(self, file_format="csv"):
+        if file_format == "csv":
+            return pd.read_csv(self.source)
+        elif file_format == "json":
+            return pd.read_json(self.source)
+        elif file_format == "sql":
+            import sqlite3
+            conn = sqlite3.connect(self.source)
+            query = "SELECT * FROM table_name"  # Modify as needed
+            return pd.read_sql_query(query, conn)
+        else:
+            raise ValueError("Unsupported file format.")

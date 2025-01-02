f69912fe7e67e6c90ddf50af5e1f501113b3a2eb
emerson <emersonelgin1111@gmail.com>
2025-01-02 23:20:13 +0530

    added dynamic graph data files

---
+++ gnn_library/Data/dynamic update.py
@@ -0,0 +1,23 @@
+class DynamicGraphUpdater:
+    def __init__(self, graph):
+        self.graph = graph
+
+    def add_nodes(self, nodes):
+        for node in nodes:
+            if node not in self.graph:
+                self.graph.add_node(node)
+
+    def add_edges(self, edges):
+        for edge in edges:
+            if not self.graph.has_edge(edge[0], edge[1]):
+                self.graph.add_edge(edge[0], edge[1])
+
+    def remove_nodes(self, nodes):
+        for node in nodes:
+            if node in self.graph:
+                self.graph.remove_node(node)
+
+    def remove_edges(self, edges):
+        for edge in edges:
+            if self.graph.has_edge(edge[0], edge[1]):
+                self.graph.remove_edge(edge[0], edge[1])

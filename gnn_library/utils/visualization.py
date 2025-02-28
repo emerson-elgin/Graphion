e8abcb8afb19c4caadb1ce0dbacb97ab2946e0a9
emerson <emersonelgin1111@gmail.com>
2025-02-28 13:05:33 +0530

    Feat(batch processing): improve neighborhood sampling. Addresses feedback from code review

--- gnn_library/utils/visualization.py
+++ gnn_library/utils/visualization.py
@@ -1,5 +1,6 @@
 # Updated: Fix visualization for performance
 class Visualization:
+# TODO: Implement additional functionality here
     """
 
     Update visualization for compatibility. Implements suggestions from research papers
